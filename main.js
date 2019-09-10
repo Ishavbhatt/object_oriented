8be6cd12f4fe43aaa4cfd4134cc53cb6e7fc903b
Ishav bhatt <ishavbhattkhaniyara.com>
2019-09-10 22:22:29 +0530

    2 method

--- main.js
+++ main.js
@@ -23,4 +23,33 @@
       // output [name: "ishav", score: 7, increment: ƒ, decrement: ƒ]
 
       // using dunder proto
-      function createUser
+    function createUser(name,score = 0){
+    var obj = {};
+    obj.__proto__.increment = function(score) {
+        return obj.score ++;
+    }
+    obj.__proto__.decrement = function(score) {
+        return obj.score --;
+    }
+    obj.name = name;
+    obj.score = score;
+    return obj;
+    }
+
+    var ishav = createUser("ishav", 4)
+
+    ishav
+
+    // output {name: "ishav", score: 4}
+
+    ishav.increment()
+
+    ishav.increment()
+
+    ishav.increment()
+
+    ishav.increment()
+    
+    ishav
+
+    // output {name: "ishav", score: 8}
